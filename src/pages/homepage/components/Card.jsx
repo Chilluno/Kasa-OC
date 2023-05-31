7127b05aec98f0e5b1c6195766b485b274f66728
Wesley Slimani <wesley.slimani@gmail.com>
2023-05-31 19:02:32 +0200

    fix: added Links to the card component

--- src/pages/homepage/components/Card.jsx
+++ src/pages/homepage/components/Card.jsx
@@ -1,9 +1,12 @@
 import React from "react"
+import {Link} from "react-router-dom"
 import "../../../styles/card.css"
 
 export const Card = ({logement}) => {
-   return <div className="card-container">
+   return <Link to={`/home/${logement.id}`}>
+      <div className="card-container">
       <img src={logement.cover} alt={logement.title} className="card-img"/>
       <h2 className="card-title">{logement.title}</h2>
    </div>
+   </Link>
 }
