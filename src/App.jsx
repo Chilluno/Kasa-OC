111949d9e76f78838761a66002b6204d39b904cc
Wesley Slimani <wesley.slimani@gmail.com>
2023-05-31 19:00:37 +0200

    feat: added Details page for housing details

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header"
 import { Homepage } from './pages/homepage/Homepage'
 import { About } from './pages/about/About'
 import { PageNotFound } from "./pages/error/PageNotFound"
+import { Details } from "./pages/details/Details"
 
 
 
@@ -17,6 +18,7 @@ function App() {
       <Route path="/" element={<Homepage />} />
       <Route path="/about" element={<About />} />
       <Route path="*" element={<PageNotFound />} />
+      <Route path="/home/:homeId" element={<Details />} />
       </Routes>
    
     </div>
