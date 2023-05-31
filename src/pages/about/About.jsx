6969560de07058b78365a6fb5f677d8f297c9aa5
Wesley Slimani <wesley.slimani@gmail.com>
2023-05-31 19:01:39 +0200

    feat: created About page with banner and dropdown components

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,5 +1,19 @@
 import React from "react"
+import abouts from "../../../about.json"
+import { Accordion } from "../../components/Accordion"
+import "../../styles/About.css"
+import { Banner } from "../../components/Banner"
+import aboutBanner from "../../assets/banner-about.png"
+
 
 export const About = () => {
-   return <h1>About</h1>
+   return <><Banner page="about" banner={aboutBanner}/>
+   <div className="about-container">
+      {
+       abouts.map((about, index) => (
+         <Accordion key={index} info={about}/>
+       ))
+      }
+   </div>
+   </>
 }
