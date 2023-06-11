aa13d4dcb152703029fc8f960d185e363b13966e
Wesley Slimani <wesley.slimani@gmail.com>
2023-06-11 19:53:36 +0200

    feat: updated the error page's content

--- src/pages/error/PageNotFound.jsx
+++ src/pages/error/PageNotFound.jsx
@@ -1,5 +1,11 @@
 import React from "react"
+import {Link} from "react-router-dom"
+import "../../styles/Error.css"
 
 export const PageNotFound = () => {
-   return <h1>Page Not Found !</h1>
+   return <div className="error-container">
+   <h1 className="error-heading">404</h1>
+   <p className="error-msg">Oups, la page que vous demandez n'existe pas</p>
+   <Link to="/" className="error-link">Retourner sur la page d'accueil</Link>
+   </div>
 }
