95118e4f25d5298a30f0df1b242dae23d6828d3a
Wesley Slimani <wesley.slimani@gmail.com>
2023-06-11 19:55:50 +0200

    feat: made the accordion component

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react"
 import "../styles/Accordion.css"
 
-export const Accordion = ({info}) => {
+export const Accordion = ({title, description}) => {
  const [display, setDisplay] = useState(false);
 
    return <div className="accordion-container">
-      <div className="accordion-title"><h3>{info.title}</h3></div>
-      <button onClick={() => setDisplay(!display)}>Click</button>
+      <div className="accordion-title"><h3>{title}</h3><button onClick={() => setDisplay(!display)}>Click</button></div>
       {  display ?
          <div className="accordion-content">
-      <p>{info.description}</p>
+      { 
+         typeof description === "string" ? <p className="accordion-description">{description}</p>
+         : description.map((desc, index) => {
+            return <p className="accordion-description" key={index}>{desc}</p>
+         })
+      }
       </div> : null}
    </div>
 }
