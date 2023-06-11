d6c7174c15b4ef54ccf4bcd5a4be2a3af1bc86cb
Wesley Slimani <wesley.slimani@gmail.com>
2023-06-11 19:56:32 +0200

    feat: made the details page for the homes

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,50 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import {useEffect} from "react"
+import { useParams, useNavigate } from "react-router-dom"
+import "../../styles/Details.css"
+import homeDetails from "../../../logements.json"
+import { Accordion } from "../../components/Accordion";
+import { Slideshow } from "./components/Slideshow";
+import { Tag } from "./components/Tag";
+import { Rating } from "./components/Rating";
 
 export const Details = () => {
-   const homeId = useParams("homeId");
-   console.log(homeId);
+   const {homeId} = useParams();
+   const navigate = useNavigate();
 
-   return <div>Details</div>
+   
+   const currentHome = homeDetails.find(detail => detail.id === homeId)
+
+if(currentHome == undefined){
+      useEffect(() => {
+         navigate("*");
+      })
+   }
+
+if(currentHome != undefined){
+   return <main>
+
+      <Slideshow slides={currentHome.pictures}/>
+      <div className="details-title-container">
+         <div className="details-title">
+         <h1>{currentHome.title}</h1> 
+         <p>{currentHome.location}</p>
+         <Tag tags={currentHome.tags}/>
+         </div>
+         <div className="details-host">
+            <div className="host-container">
+            <p>{currentHome.host.name}</p>
+            <img className="details-host-image" src={currentHome.host.picture} />
+            </div>
+            <Rating rate={currentHome.rating}/>
+         </div>
+      </div>
+
+
+      <div className="details-dropdown-container">
+      <Accordion title="Description" description={currentHome.description}/>
+      <Accordion title="Équipements" description={currentHome.equipments}/>
+      </div>
+      </main>
+   }
 } 
