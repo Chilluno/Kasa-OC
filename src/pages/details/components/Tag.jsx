efb2c28ca0a563c5d9faeffe9d71a7b06484f443
Wesley Slimani <wesley.slimani@gmail.com>
2023-06-11 19:52:52 +0200

    feat: created the tag component

---
+++ src/pages/details/components/Tag.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../../styles/Tag.css"
+
+export const Tag = ({tags}) => {
+
+
+   return <div className="tags-container">
+   {
+      tags.map((tag, index) => <div className="tag" key={index}>{tag}</div>)
+   }
+   </div>
+}
