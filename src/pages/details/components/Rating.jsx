1abe33a6afc1160fa3fb2025f84a2b9a4dce60df
Wesley Slimani <wesley.slimani@gmail.com>
2023-06-11 19:53:51 +0200

    feat: created the star rating component

---
+++ src/pages/details/components/Rating.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import "../../../styles/Rating.css"
+import starOn from "../../../assets/star-rating-on.png"
+import starOff from "../../../assets/star-rating-off.png"
+
+export const Rating = (props) => {
+
+const currentRating = props.rate;
+
+
+
+   return (<div className="star-rating">{
+      [...Array(5)].map((star, index) => {
+         return (
+            <img key={index} className="star" src={index < currentRating ?  starOn : starOff} />
+         )
+      })
+   }
+    </div>
+   )
+}
